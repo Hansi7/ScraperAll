77cf210b125ee22816897d20789367c74a8ab441
hansi <hansi-go@163.com>
2016-11-17 00:15:34 +0800

    new program scrapyworker v2

---
+++ packages/phantomjs/Readme.md
@@ -0,0 +1,1 @@
+Copy This To Debug Directory
---
+++ packages/phantomjs/test.js
@@ -0,0 +1,23 @@
+var page = require('webpage').create(),
+system=require('system');
+//var url = 'http://www.iqiyi.com/v_19rr98odxg.html';
+var url = system.args[1];
+page.open(url, function (status)
+{
+    if (status != "success")
+    {
+        console.log('FAIL to load the address');
+        phantom.exit();
+    }
+    page.evaluate(function()
+    {
+    });
+
+    window.setTimeout(function ()
+    {
+        page.render("333.png");
+        console.log(page.content);
+        phantom.exit();
+    }, 15000);
+
+});
